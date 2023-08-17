cdca61e25a5dd47871efb4f31c91bb03b7d3ed1e
zero <dukepandme@gmail.com>
2023-08-17 16:00:10 +0800

    error、Image、reader example

--- interface/exercise-stringer.go
+++ interface/exercise-stringer.go
@@ -4,7 +4,6 @@ import "fmt"
 
 type IPAddr [4]byte
 
-// TODO: 给 IPAddr 添加一个 "String() string" 方法
 func (IP IPAddr) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", IP[0], IP[1], IP[2], IP[3])
 }
--- reader/exercise-reader.go
+++ reader/exercise-reader.go
@@ -4,7 +4,6 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
-// TODO: 给 MyReader 添加一个 Read([]byte) (int, error) 方法
 func (r MyReader) Read(b []byte) (int, error) {
 	b[0] = 'A'
 	return 1, nil
